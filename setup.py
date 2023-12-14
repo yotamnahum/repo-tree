7ba14b1c318c0d61ba5505fd748eee0ea5d586b2
yotamnahum <yotamnahum@gmail.com>
2023-12-14 03:49:20 +0200

    Add RepositoryTree class and display_tree function
    This commit adds the RepositoryTree class and the display_tree function to the repo_tree package. The RepositoryTree class represents a repository tree structure and the display_tree function generates and displays the directory tree.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=open('requirements.txt').read().splitlines(),
     entry_points={
         'console_scripts': [
-            'repo-tree=repo_tree.repository_tree:main',  # Assuming main() is your entry function
+            'repo_tree=repo_tree.repository_tree:main',  # Assuming main() is your entry function
         ],
     },
 )
